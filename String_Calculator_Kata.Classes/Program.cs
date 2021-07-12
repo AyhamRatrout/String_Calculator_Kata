7c6ddc2e8d43a07db14b3d3283f6d2cfd140d6e2
AyhamRatrout <arr90@case.edu>
2021-07-12 13:31:22 +0300

    Initial Commit - Create Project

---
+++ String_Calculator_Kata.Classes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace String_Calculator_Kata.Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
